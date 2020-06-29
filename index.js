a9d6beac40bf68df62765452c389b3fa130fad5a
root <root@DESKTOP-HV1IFTU.localdomain>
2020-06-29 11:40:06 -0500

    add web server api

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const {graphql, buildSchema} = require('graphql')
+const express = require('express')
+const gdlMiddleware = require('express-graphql')
+
+const app = express()
+const port = process.env.port || 3008
 
 // definiendo el squema
-
 const schema =  buildSchema(`
     type Query {
         hello : String
@@ -19,8 +23,14 @@ const resolvers = {
         return 'Saludos a todos'
     }
 }
-// ejecutando el query hello
-graphql(schema, '{saludo}', resolvers)
-    .then(data => {
-        console.table(data);
-    }).catch(err => console.error(err) )
+
+app.use('/api', gdlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listenign at http://localhost:${port}/api`);
+})
+
