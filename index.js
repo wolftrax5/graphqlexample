6459f59c735276f87ac75bcd75abb222506f89cb
root <root@DESKTOP-HV1IFTU.localdomain>
2020-06-29 14:05:57 -0500

    add customs queries

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express')
 const gdlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+// configura los resolvers
+const resolvers = require('./lib/resolves')
 
 const app = express()
 const port = process.env.port || 3008
@@ -15,15 +17,6 @@ const schema =  buildSchema(
         join(__dirname, 'lib', 'schema.graphql'),
         'utf-8')
     )
-// configura los resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hello World'
-    },
-    saludo: () => {
-        return 'Saludos a todos'
-    }
-}
 
 app.use('/api', gdlMiddleware({
     schema: schema,
