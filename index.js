d6c593e08e55440d6d654dfb198db411a75dc525
root <root@DESKTOP-HV1IFTU.localdomain>
2020-06-29 13:37:55 -0500

    add schema files

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 'use strict'
 
-const {graphql, buildSchema} = require('graphql')
+const {buildSchema} = require('graphql')
 const express = require('express')
 const gdlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
 const app = express()
 const port = process.env.port || 3008
 
 // definiendo el squema
-const schema =  buildSchema(`
-    type Query {
-        hello : String
-        saludo: String
-    }
-`)
+const schema =  buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8')
+    )
 // configura los resolvers
 const resolvers = {
     hello: () => {
