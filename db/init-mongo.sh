724a26a9db3ab8bc2f499a1e79d65bddbe364189
root <root@DESKTOP-HV1IFTU.localdomain>
2020-06-30 15:20:28 -0500

    add set Base Data

--- db/init-mongo.sh
+++ db/init-mongo.sh
@@ -11,3 +11,23 @@ mongo ${DB_NAME} \
         -p ${MONGO_INITDB_ROOT_PASSWORD} \
         --authenticationDatabase admin \
         --eval "db.createUser({user: '${DB_USER}', pwd: '${DB_PASSWD}', roles:[{role:'dbOwner', db: '${DB_NAME}'}]});"
+
+echo 'Set Data'
+mongo ${DB_NAME} \
+        --host localhost \
+        --port ${DB_PORT} \
+        -u ${MONGO_INITDB_ROOT_USERNAME} \
+        -p ${MONGO_INITDB_ROOT_PASSWORD} \
+        --authenticationDatabase admin \
+        --eval "db.getCollection('courses').insertMany([{
+    title: 'Node JS',
+    teacher: 'Alejandro Medina',
+    descrition: 'Basicos de Node',
+    topic: 'JS',
+    },
+    {
+        title: 'React JS',
+        teacher: 'Alejandro Medina',
+        descrition: 'Basicos de React',
+        topic: 'JS',
+    }]);"
